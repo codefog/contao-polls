aba50f053575630a3f39daaa4f3cae6b663dcc8a
lionel-m <lionel.maccaud@gmail.com>
2013-12-29 20:04:23 +0100

    French translation

---
+++ languages/fr/tl_module.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * polls extension for Contao Open Source CMS
+ *
+ * Copyright (C) 2014 Codefog
+ *
+ * @package polls
+ * @author  Codefog <http://codefog.pl>
+ * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
+ * Translator: Lionel Maccaud (https://github.com/lionel-m)
+ * @license LGPL
+ */
+
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['poll_active']   = array('Éléments actifs', 'Ici vous pouvez choisir la façon dont les sondages actifs sont gérés.');
+$GLOBALS['TL_LANG']['tl_module']['poll_visible']  = array('Éléments visibles', 'Ici vous pouvez choisir la façon dont les sondages visibles sont gérés.');
+$GLOBALS['TL_LANG']['tl_module']['poll_featured'] = array('Éléments en vedette', 'Ici vous pouvez choisir la façon dont les sondages en vedette sont gérés.');
+
+
+/**
+ * Reference
+ */
+$GLOBALS['TL_LANG']['tl_module']['poll_active']['all']   = 'Tous les éléments';
+$GLOBALS['TL_LANG']['tl_module']['poll_active']['yes']   = 'Seuls les éléments actifs';
+$GLOBALS['TL_LANG']['tl_module']['poll_active']['no']    = 'Seuls les éléments inactifs';
+$GLOBALS['TL_LANG']['tl_module']['poll_visible']['all']  = 'Tous les éléments';
+$GLOBALS['TL_LANG']['tl_module']['poll_visible']['yes']  = 'Seuls les éléments visibles';
+$GLOBALS['TL_LANG']['tl_module']['poll_visible']['no']   = 'Seuls les éléments invisibles';
+$GLOBALS['TL_LANG']['tl_module']['poll_featured']['all'] = 'Tous les éléments';
+$GLOBALS['TL_LANG']['tl_module']['poll_featured']['yes'] = 'Seuls les éléments en vedette';
+$GLOBALS['TL_LANG']['tl_module']['poll_featured']['no']  = 'Seuls les éléments standards';
